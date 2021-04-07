eede92f6974008f903f8c4c0099fc7d4c9aa0b75
Logan Gore <gorel@fb.com>
2021-04-07 15:09:11 -0700

    Hard-code US mappings to fix build error

--- src/emojipasta/util/emoji.py
+++ src/emojipasta/util/emoji.py
@@ -4,7 +4,8 @@ Common emoji code.
 
 import emoji
 
-EMOJIS = set(emoji.emojize(emoji_code) for emoji_code in emoji.UNICODE_EMOJI.values())
+# Hard-code to take US emoji mappings
+EMOJIS = set(emoji.emojize(emoji_code) for emoji_code in list(emoji.UNICODE_EMOJI.values())[0])
 
 def is_emoji(c):
     return c in EMOJIS
